aec6841ace736e17910f99b7678642600f6f62df
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2022-05-05 22:24:34 -0300

    Update QR.js

--- screens/QR.js
+++ screens/QR.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 export default function QR({ navigation }) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
@@ -24,50 +24,15 @@ export default function QR({ navigation }) {
     }
 
   };
-  const storeData = async (value) => {
-    try {
-      const jsonValue = JSON.stringify(value)
-      await AsyncStorage.setItem('@data_user', jsonValue)
-    } catch (e) {
-      console.log('erro ao efetuar o estado ' + e)
-    }
-  }
 
-  function Login(value) {
-
-    axios
-      .get("https://birdra1n.x10.bz/IFPI_PORTARIA/api/user/login/", {
-        params: {
-          method: 'QRCode',
-          QRData: value,
-        },
-      })
-      .then(function (response) {
-        storeData(response.data)
-
-        if (response.data.token_session !== undefined) {
-          navigation.navigate('HomeScreen');
-        }
-
-
-      });
-
-  }
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
 
   return (
     <View style={styles.container}>
       <View style={styles.box}>
-      <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
       </View>
     </View>
   );
@@ -81,7 +46,7 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     alignItems: 'center'
   },
-  box:{
+  box: {
     width: '60%',
     height: '80%'
   }
